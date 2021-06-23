81ffc8177d7eb6d3f50f6b96a9a15effabc1986c
Muhammed Çağrı Çalışkan <caliskan1998@gmail.com>
2021-06-23 21:24:04 +0300

    User add

---
+++ BootcampFinal/Controllers/AccountController.cs
@@ -0,0 +1,55 @@
+﻿using BootcampFinal.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using X.PagedList;
+
+namespace BootcampFinal.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ModelContext _db;
+
+        public AccountController(ModelContext db)
+        {
+            _db = db;
+        }
+
+
+
+        public IActionResult Index(int page = 1, string search = "")
+        {
+            var users = _db.Users.Where(x => x.UserRole == UserRole.Resident);
+
+            if(search != "")
+            {
+                users = users.Where(x => x.Name.Contains(search) || x.LastName.Contains(search));
+
+                ViewBag.search = search;
+                ViewBag.searhCount = users.Count();
+            }
+
+            users = users.OrderByDescending(n => n.Id);
+            ViewBag.page = page;
+
+            return View(users.ToPagedList(page,10));
+        }
+
+        [HttpPost]
+        public IActionResult AddUser(User user)
+        {
+           
+            if( user != null)
+            {
+                _db.Add(user);
+                _db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ BootcampFinal/ViewModels/NotificationViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BootcampFinal.ViewModels
+{
+    public class NotificationViewModel
+    {
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public NotificationType NotificationType { get; set; }
+
+        public string SerializeNotification()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+
+        public static NotificationViewModel GetNotificationFromJson(string notificationJson)
+        {
+            return JsonConvert.DeserializeObject<NotificationViewModel>(notificationJson);
+        }
+    }
+
+
+    public enum NotificationType
+    {
+        success,
+        info,
+        warning,
+        danger
+    }
+}
