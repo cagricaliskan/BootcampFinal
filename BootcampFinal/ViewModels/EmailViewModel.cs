6a5a6c717c12b3c7c3b476c158a7bd1b1949fb50
Muhammed Çağrı Çalışkan <caliskan1998@gmail.com>
2021-07-04 23:58:27 +0300

    email cont

---
+++ BootcampFinal/ViewModels/EmailViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BootcampFinal.ViewModels
+{
+    public class EmailViewModel
+    {
+        public string Text { get; set; }
+        public string Link { get; set; }
+    }
+}
---
+++ BootcampFinal/ViewString/ViewToStringRenderer.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace BootcampFinal.ViewString
+{
+    public static class ViewToStringRenderer
+    {
+        public static async Task<string> RenderViewToStringAsync<TModel>(IServiceProvider requestServices, string viewName, TModel model)
+        {
+            var viewEngine = requestServices.GetRequiredService(typeof(IRazorViewEngine)) as IRazorViewEngine;
+            ViewEngineResult viewEngineResult = viewEngine.GetView(null, viewName, false);
+            if (viewEngineResult.View == null)
+            {
+                throw new Exception("Could not find the View file. Searched locations:\r\n" + string.Join("\r\n", viewEngineResult.SearchedLocations));
+            }
+            else
+            {
+                IView view = viewEngineResult.View;
+                var httpContextAccessor = (IHttpContextAccessor)requestServices.GetRequiredService(typeof(IHttpContextAccessor));
+                var actionContext = new ActionContext(httpContextAccessor.HttpContext, new RouteData(), new ActionDescriptor());
+                var tempDataProvider = requestServices.GetRequiredService(typeof(ITempDataProvider)) as ITempDataProvider;
+
+                using var outputStringWriter = new StringWriter();
+                var viewContext = new ViewContext(
+                    actionContext,
+                    view,
+                    new ViewDataDictionary<TModel>(new EmptyModelMetadataProvider(), new ModelStateDictionary()) { Model = model },
+                    new TempDataDictionary(actionContext.HttpContext, tempDataProvider),
+                    outputStringWriter,
+                    new HtmlHelperOptions());
+
+                await view.RenderAsync(viewContext);
+
+                return outputStringWriter.ToString();
+            }
+        }
+    }
+}
